f7b7a19f338c668cc7a69757a84391c592e323aa
Nanguti <g.nanguti@gmail.com>
2023-09-26 00:34:07 +0300

    Navigation menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,15 +3,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-import {
-  faBitbucket,
-  faLinkedin,
-  faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const links = [
-  { href: "/", text: "The Syntax" },
   { href: "/jobs", text: "Jobs" },
   { href: "/blog", text: "Blog" },
   { href: "/projects", text: "Projects" },
@@ -24,19 +18,16 @@ function Header() {
     <nav className="fixed z-50 w-full bg-white top-0 flex flex-wrap items-center justify-between px-2 py-3 shadow-lg">
       <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-          {links.map((link, index) => (
-            <Link
-              key={index}
-              className={`${
-                link.href === path
-                  ? "text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-yellow-700 "
-                  : "text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-slate-700"
-              }`}
-              href={link.href}
-            >
-              {link.text}
-            </Link>
-          ))}
+          <Link
+            className={`${
+              "/" === path
+                ? "text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-yellow-700 "
+                : "text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-slate-700"
+            }`}
+            href="/"
+          >
+            The Syntax
+          </Link>
           <button
             className="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
             type="button"
@@ -49,35 +40,20 @@ function Header() {
           id="example-navbar-danger"
         >
           <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
-            <li className="nav-item">
-              <a
-                href="https://twitter.com/ProfNanguti"
-                className="px-3 py-2 flex items-center text-xs uppercase font-bold text-slate-700 hover:text-slate-500"
-                target="_blank"
-              >
-                <FontAwesomeIcon icon={faTwitter} style={{ fontSize: 20 }} />
-                <span className="ml-2">Tweet</span>
-              </a>
-            </li>
-
-            <li className="nav-item">
-              <a
-                className="download-button px-3 py-2 flex items-center text-xs uppercase font-bold text-slate-700 hover:text-slate-500"
-                href="https://bitbucket.org/nanguti/workspace/overview/"
-              >
-                <FontAwesomeIcon icon={faBitbucket} style={{ fontSize: 20 }} />
-                <span className="ml-2">Bitbucket</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="https://www.linkedin.com/in/kevin-w-wanyonyi/"
-                className="download-button px-3 py-2 flex items-center text-xs uppercase font-bold text-slate-700 hover:text-slate-500"
-              >
-                <FontAwesomeIcon icon={faLinkedin} style={{ fontSize: 20 }} />
-                <span className="ml-2">LinkedIn</span>
-              </a>
-            </li>
+            {links.map((link, index) => (
+              <li className="nav-item" key={index}>
+                <Link
+                  href={link.href}
+                  className={`${
+                    link.href === path
+                      ? "text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-yellow-700 "
+                      : "text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-slate-700"
+                  }`}
+                >
+                  <span className="ml-2">{link.text}</span>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
