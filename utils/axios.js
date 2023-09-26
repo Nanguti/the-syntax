59327643befabe209d60c88c1fb68e117953be84
Nanguti <g.nanguti@gmail.com>
2023-09-26 04:09:51 +0300

    api link

--- utils/axios.js
+++ utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "https://thesyntax.nanguti.co.ke/api",
   headers: {
     "Content-Type": "application/json",
   },
